874e95a5b25d0e161e21d0030e1cb60fce1e42c8
keith hill <keith.hill@metastock.com>
2022-07-02 06:50:57 -0600

    after install, run dev, overview component

---
+++ components/Overview.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Overview flex flex-col md:flex-row">
+    <div class="main w-3/5">main</div>
+    <div class="secondary w-2/5">secondary</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
-	<div class="container mx-auto mt-10 text-xl text-center text-gray-600">
-		<div class="flex flex-col items-center justify-center w-full bg-gray-100 rounded-md shadow-md">
+	<div class="container mx-4 sm:mx-auto ">
+		<div
+			class="flex flex-col items-center justify-center w-full mt-10  bg-gray-100 rounded-md shadow-md  text-center text-gray-600"
+		>
 			<div class="flex flex-col justify-start w-full p-4">
-				<h1 class="py-3 text-3xl border-b-2 font-montserrat">Welcome to the Nuxt Project Template</h1>
+				<h1 class="py-3 text-3xl border-b-2 font-montserrat text-red-700">Welcome to the Nuxt Project Template</h1>
 				<p class="py-3">This is the the Nuxt Project Template</p>
 				<p>
 					This message shows that the template is well configured and running. The site is still under construction and
@@ -10,11 +12,17 @@
 				</p>
 			</div>
 		</div>
+
+		<hr>
+
+		<Overview></Overview>
 	</div>
 </template>
 
 <script>
+import Overview from '~/components/Overview.vue';
 export default {
-	name: 'Index',
+	name: "Index",
+	components: { Overview }
 };
 </script>
